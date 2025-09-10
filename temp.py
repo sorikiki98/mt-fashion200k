b97829a71702418304509535f50dd8e767c59132
gpusystem <gpuadmin@node1.cluster.local>
2025-09-10 13:46:07 +0000

    [ADD] total image counting

--- temp.py
+++ temp.py
@@ -1,27 +1,19 @@
-from transformers import BertTokenizer
 from pathlib import Path
 import json
 
-from lavis.models import load_model_and_preprocess
 
-json_path = "/mnt/c/Users/user/PycharmProjects/mtcir/mt_dataset"
+json_path = "/home/gpuadmin/dasol/mt-fashion200k/mt_dataset"
 
-with open(Path(json_path) / "train_convergence.json",
+with open(Path(json_path) / "test_convergence.json",
           encoding="utf-8") as convergence:
     convergence = json.load(convergence)
-with open(Path(json_path) / "train_rollback.json", encoding="utf-8") as rollback:
-    rollback = json.load(rollback)
-with open(Path(json_path) / "train_combination.json", encoding="utf-8") as combination:
-    combination = json.load(combination)
+#with open(Path(json_path) / "train_rollback.json", encoding="utf-8") as rollback:
+#    rollback = json.load(rollback)
+#with open(Path(json_path) / "train_combination.json", encoding="utf-8") as combination:
+#    combination = json.load(combination)
 
 transactions = convergence
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", truncation_side="right")
-tokenizer.add_special_tokens({"bos_token": "[DEC]"})
-with open("config.json", "r", encoding="utf-8") as f:
-    config = json.load(f)
-blip_model, _, txt_processors = load_model_and_preprocess(
-    name=config["blip_model_name"], model_type="pretrain", is_eval=False, device="cuda"
-)
+img_ids = list()
 
 for sample in transactions:
     n_turns = sample["n_turns"]
@@ -30,41 +22,11 @@ for sample in transactions:
     for i in range(1, 6):  # turn-1 ~ turn-5
         if i <= n_turns:
             tar_key = f"turn-{i}"
-            mod = sample[tar_key]["mod_str"]
-            processed_mod = txt_processors["eval"](mod)
-            tar_caption = sample[tar_key]["target_caption"]
-            processed_tar_caption = txt_processors["eval"](tar_caption)
-            tar_captions.append(processed_tar_caption)
-            mods.append(processed_mod)
-        else:
-            processed_none = txt_processors["eval"]("none")
-            tar_captions.append(processed_none)
-            mods.append(processed_none)
+            src_img_id = sample[tar_key]["source_img_id"]
+            tar_img_id = sample[tar_key]["target_img_id"]
+        if src_img_id not in img_ids:
+            img_ids.append(src_img_id)
+        if tar_img_id not in img_ids:
+            img_ids.append(tar_img_id)
 
-        turn_key = f"turn-{i}"
-    try:
-        cap_tokens = tokenizer(
-            tar_captions,
-            padding="max_length",
-            truncation=True,
-            max_length=40,
-            return_tensors="pt",
-            add_special_tokens=False
-        )
-    except Exception as e:
-        print("[Tokenizer Error] cap_tokens failed")
-        print("Input tar_captions:", tar_captions)
-        raise e
-    try:
-        mod_tokens = tokenizer(
-            mods,
-            padding="max_length",
-            truncation=True,
-            max_length=40,
-            return_tensors="pt",
-            add_special_tokens=False
-        )
-    except Exception as e:
-        print("[Tokenizer Error] mod_tokens failed")
-        print("Input mods:", mods)
-        raise e
+print(len(img_ids))
